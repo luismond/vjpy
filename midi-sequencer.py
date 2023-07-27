9543834e5e4e3b68518b837d5467d1a611a10996
Luis Mondragon <luismond@gmail.com>
2023-07-27 11:00:36 -0600

    add note values as property of the sequencer class

--- midi-sequencer.py
+++ midi-sequencer.py
@@ -6,6 +6,9 @@ import mido
 from mido import Message
 from time import sleep
 
+# GLOBAL VARIABLES
+OUTPORT = mido.open_output()
+BPM = 60
 
 # Data Classes
 
@@ -29,28 +32,6 @@ class Drum(BaseModel):
 class NoteValue(BaseModel):
     name: str
     relative_value: float
-    repr_: str
-
-
-# GLOBAL VARIABLES
-OUTPORT = mido.open_output()
-
-
-def get_bpm(int_):
-    return int_/60
-
-
-BPM = get_bpm(120)
-
-
-NOTE_VALUES = {
-    '1': NoteValue(name='whole_note', relative_value=1.0, repr_='1'),
-    '1/2': NoteValue(name='half_note', relative_value=0.5, repr_='1/2'),
-    '1/4': NoteValue(name='quarter_note', relative_value=0.25, repr_='1/4'),
-    '1/8': NoteValue(name='eigth_note', relative_value=0.125, repr_='1/8'),
-    '1/16': NoteValue(name='sixteenth_note', relative_value=0.0625, repr_='1/16'),
-    '1/32': NoteValue(name='thirty-second_note', relative_value=0.03125, repr_='1/32')
-    }
 
 
 DRUMKIT = Drumkit(
@@ -68,6 +49,54 @@ DRUMKIT = Drumkit(
     )
 
 
+class MidiSequencer:
+    def __init__(self):
+        self.BPM = BPM
+        self.note_duration = self.BPM/60
+
+    def play_note(self, note, duration=0, velocity=50):
+        msg = Message('note_on',  note=note, velocity=velocity)
+        OUTPORT.send(msg)
+        sleep(duration)
+
+    def play_drum(self, drum_name, duration=0):
+        drum_note = DRUMKIT.drums[drum_name].note
+        self.play_note(note=drum_note, duration=duration)
+
+    @staticmethod
+    def play_silence(duration=0):
+        sleep(duration)
+
+    def play_pattern(self, pattern):
+        note_value = self.note_values['1/4'].relative_value / self.note_duration
+        for beat in pattern:
+            if beat != '|':
+                if beat == '.':
+                    self.play_silence(duration=note_value)
+                else:
+                    drum_name = [
+                        drum.name for drum in DRUMKIT.drums.values()
+                        if drum.short_hand == beat
+                        ][0]  # simplify
+                    self.play_drum(drum_name=drum_name, duration=note_value)
+
+    @property
+    def note_values(self):
+        note_values = {
+            '1': NoteValue(name='whole_note', relative_value=1.0),
+            '1/2': NoteValue(name='half_note', relative_value=0.5),
+            '1/4': NoteValue(name='quarter_note', relative_value=0.25),
+            '1/8': NoteValue(name='eigth_note', relative_value=0.125),
+            '1/16': NoteValue(name='sixteenth_note', relative_value=0.0625),
+            '1/32': NoteValue(name='thirty-second_note', relative_value=0.03125)
+            }
+        return note_values
+
+
+seq = MidiSequencer()
+
+
+# Pattern example
 PATTERNS = {
     '1': Pattern(
         timeline='01..|02..|03..|04..|',
@@ -84,43 +113,5 @@ PATTERNS = {
     }
 
 
-class MidiSequencer:
-    def __init__(self):
-        pass
-
-    def play_note(self, note, duration=0, velocity=50):
-        msg = Message('note_on',  note=note, velocity=velocity)
-        OUTPORT.send(msg)
-        sleep(duration)
-
-    def play_drum(self, drum_name, duration=0):
-        drum_note = DRUMKIT.drums[drum_name].note
-        self.play_note(note=drum_note, duration=duration)
-
-    @staticmethod
-    def play_silence(duration=0):
-        sleep(duration)
-
-    def play_pattern(self, pattern):
-        note_value = NOTE_VALUES['1/4'].relative_value / BPM
-        for beat in pattern:
-            if beat != '|':
-                if beat == '.':
-                    self.play_silence(duration=note_value)
-                else:
-                    drum_name = [
-                        drum.name for drum in DRUMKIT.drums.values()
-                        if drum.short_hand == beat
-                        ][0]  # simplify
-                    self.play_drum(drum_name=drum_name, duration=note_value)
-
-
-seq = MidiSequencer()
-
 for pattern_num in PATTERNS:
     seq.play_pattern(PATTERNS[pattern_num].pattern)
-
-"""
-verbs: play, record
-nouns: beat, note, silence, message, pattern, drum, value
-"""
