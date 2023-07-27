26b5e67d886b8fc356aa19d98311a7399777a2d9
Luis Mondragon <luismond@gmail.com>
2023-07-27 08:02:30 -0600

    organize data classes, objects and functions

--- midi-sequencer.py
+++ midi-sequencer.py
@@ -1,22 +1,25 @@
-# -*- coding: utf-8 -*-
-"""MIDI sequencer."""
+# # -*- coding: utf-8 -*-
+# """MIDI sequencer."""
 
+from pydantic import BaseModel
 import mido
 from mido import Message
 from time import sleep
-from pydantic import BaseModel
+
+# # todo: define proper note durations
+# # todo: define fundamental sequencing objects and actions
+
+# Data Classes
 
 
-"""
-pydantic example
+class Pattern(BaseModel):
+    timeline: str
+    pattern: str
 
-class Delivery(BaseModel):
-    timestamp: datetime
-    dimensions: Tuple[int, int]
-"""
 
-# Open port
-outport = mido.open_output()
+class Drumkit(BaseModel):
+    name: str
+    drums: dict
 
 
 class Drum(BaseModel):
@@ -25,10 +28,19 @@ class Drum(BaseModel):
     short_hand: str
 
 
-class Drumkit(BaseModel):
-    name: str
-    drums: dict
+# Dicts
 
+note_relative_values = {
+    'whole_note': 1,
+    'half_note': .5,
+    'quarter_note': .25,
+    'eigth_note': .125,
+    'sixteenth_note': 0.0625,
+    'thirty_second_note': 0.03125
+    }
+
+
+# Objects
 
 drumkit = Drumkit(
     name='TR808EmulationKit',
@@ -44,43 +56,24 @@ drumkit = Drumkit(
         }
     )
 
+outport = mido.open_output()
 
-# todo: define proper note durations
-# todo: define fundamental sequencing objects and actions
+patterns = {
+    '1': Pattern(
+        timeline='01...|02...|03...|04...|',
+        pattern='kk..|s.hh|kk..|swht|'),
+    '2': Pattern(
+        timeline='05...|06...|07...|08...|',
+        pattern='kk..|s.hh|kk..|s.gg|'),
+    '3': Pattern(
+        timeline='09...|10...|11...|12...|',
+        pattern='kk..|s.hh|kk..|swht|'),
+    '4': Pattern(
+        timeline='13...|14...|15...|16...|',
+        pattern='kk..|shhh|kk..|ssss|')
+    }
 
-
-def tempo():
-    pass
-
-
-# def pattern():
-#     pass
-
-
-def instrument():
-    pass
-
-
-def note_values():
-    pass
-
-
-def bpm():
-    "rel value 1 = 1 second = 60 bpm"
-    pass
-
-
-def time_signature():
-    "beats / note value (4/4)"
-    pass
-
-
-def beat():
-    pass
-
-
-def velocity():
-    pass
+# Functions
 
 
 def play_note(note, duration=0, velocity=50):
@@ -89,67 +82,13 @@ def play_note(note, duration=0, velocity=50):
     sleep(duration)
 
 
-def play_silence(duration=0):
-    sleep(duration)
-
-
 def play_drum(drum_name, duration):
     drum_note = drumkit.drums[drum_name].note
     play_note(note=drum_note, duration=duration)
 
 
-note_relative_values = {
-    'whole_note': 1,
-    'half_note': .5,
-    'quarter_note': .25,
-    'eigth_note': .125,
-    'sixteenth_note': 0.0625,
-    'thirty_second_note': 0.03125
-    }
-
-
-def time_line():
-    pass
-
-
-bar_resolutions = {
-    '1/4': '....',
-    '1/8': '........',
-    '1/16': '................'
-    }
-
-
-def get_bar(resolution):
-    return bar_resolutions[resolution]
-
-
-bar = get_bar('1/4')
-
-bars = [get_bar('1/4') for n in range(4)]
-
-patterns = {
-    '1': {
-        'timeline': '01...|02...|03...|04...|',
-        'pattern':  'kk..|s.hh|kk..|swht|'
-        },
-    '2': {
-        'timeline': '05...|06...|07...|08...|',
-        'pattern':  'kk..|s.hh|kk..|s.gg|'
-        },
-    '3': {
-        'timeline': '09...|10...|11...|12...|',
-        'pattern':  'kk..|s.hh|kk..|swtt|'
-        },
-    '4': {
-        'timeline': '13...|14...|15...|16...|',
-        'pattern':  'kk..|shhh|kk..|ssss|'
-        },
-    }
-
-
-short_hand = dict()
-for d in drumkit.drums:
-    short_hand[drumkit.drums[d].short_hand] = drumkit.drums[d].name
+def play_silence(duration=0):
+    sleep(duration)
 
 
 def parse_pattern(pattern):
@@ -167,5 +106,65 @@ def parse_pattern(pattern):
                 play_drum(drum_name=drum_name, duration=note_value)
 
 
-for pattern in patterns:
-    parse_pattern(patterns[pattern]['pattern'])
+for pattern_n in patterns:
+    parse_pattern(patterns[pattern_n].pattern)
+
+# %% to_do
+
+# def tempo():
+#     pass
+
+
+# # def pattern():
+# #     pass
+
+
+# # def instrument():
+# #     pass
+
+
+# # def note_values():
+# #     pass
+
+
+# # def bpm():
+# #     "rel value 1 = 1 second = 60 bpm"
+# #     pass
+
+
+# # def time_signature():
+# #     "beats / note value (4/4)"
+# #     pass
+
+
+# # def beat():
+# #     pass
+
+
+# # def velocity():
+# #     pass
+
+
+# def time_line():
+#     pass
+
+
+# bar_resolutions = {
+#     '1/4': '....',
+#     '1/8': '........',
+#     '1/16': '................'
+#     }
+
+
+# def get_bar(resolution):
+#     return bar_resolutions[resolution]
+
+
+# bar = get_bar('1/4')
+
+# bars = [get_bar('1/4') for n in range(4)]
+
+
+# short_hand = dict()
+# for d in drumkit.drums:
+#     short_hand[drumkit.drums[d].short_hand] = drumkit.drums[d].name
