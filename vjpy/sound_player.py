c5c05d6c6cd6a5a4868059d83982d7290872d396
Luis Mondragon <luismond@gmail.com>
2023-07-27 23:14:59 -0600

    test pyaudio as backend for a sound player

--- vjpy/sound_player.py
+++ vjpy/sound_player.py
@@ -1,13 +1,37 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """sound player"""
+import wave
+import sys
+import pyaudio
+from time import sleep 
 
-import os
-from playsound import playsound 
+CHUNK = 1024
 
 sound_dir = 'wavs/my808kit'
-sounds = [f for f in os.listdir(sound_dir)]
-sound = 'hat.wav'
+sound = f'{sound_dir}/hat.wav'
+
+
+def play(sound):
+    with wave.open(sound, 'rb') as wf:
+        # Instantiate PyAudio and initialize PortAudio system resources (1)
+        p = pyaudio.PyAudio()
+    
+        # Open stream (2)
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True)
+    
+        # Play samples from the wave file (3)
+        while len(data := wf.readframes(CHUNK)):  # Requires Python 3.8+ for :=
+            stream.write(data)
+    
+        # Close stream (4)
+        stream.close()
+    
+        # Release PortAudio system resources (5)
+        p.terminate()
+        
 
 for n in range(8):
-    playsound(f'{sound_dir}/{sound}')
+    play(sound)
+    sleep(.125)
