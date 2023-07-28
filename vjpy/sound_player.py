0d7a7c3d84ab50334ae40cc62a966f4731c9132d
Luis Mondragon <luismond@gmail.com>
2023-07-28 02:31:23 -0600

    test scipy as backend for audio player

--- vjpy/sound_player.py
+++ vjpy/sound_player.py
@@ -1,37 +1,9 @@
 """sound player"""
-import wave
-import sys
-import pyaudio
-from time import sleep 
 
-CHUNK = 1024
-
-sound_dir = 'wavs/my808kit'
-sound = f'{sound_dir}/hat.wav'
-
-
-def play(sound):
-    with wave.open(sound, 'rb') as wf:
-        # Instantiate PyAudio and initialize PortAudio system resources (1)
-        p = pyaudio.PyAudio()
-    
-        # Open stream (2)
-        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                        channels=wf.getnchannels(),
-                        rate=wf.getframerate(),
-                        output=True)
-    
-        # Play samples from the wave file (3)
-        while len(data := wf.readframes(CHUNK)):  # Requires Python 3.8+ for :=
-            stream.write(data)
-    
-        # Close stream (4)
-        stream.close()
-    
-        # Release PortAudio system resources (5)
-        p.terminate()
-        
-
-for n in range(8):
-    play(sound)
-    sleep(.125)
+from scipy.io.wavfile import write
+import numpy as np
+samplerate = 44100; fs = 50
+t = np.linspace(0., 3., samplerate)
+amplitude = np.iinfo(np.int16).max
+data = amplitude * np.sin(2. * np.pi * fs * t)
+write("example.wav", samplerate, data.astype(np.int16))
