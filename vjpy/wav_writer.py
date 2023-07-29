4736420ab9c9711b6353f30991292bf5a01c8750
Luis Mondragon <luismond@gmail.com>
2023-07-29 03:49:43 -0600

    add wave definitions

--- vjpy/wav_writer.py
+++ vjpy/wav_writer.py
@@ -6,16 +6,39 @@ from scipy.io.wavfile import write
 import numpy as np
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
-
+from playsound import playsound
+#%%
 # write wav
-# create a 100Hz sine wave, sampled at 44100Hz. Write to 16-bit PCM, Mono.
+# create a 100Hz sine wave, sampled at 44100Hz. 
+# Write to 16-bit PCM, Mono.
 wav_filename = 'wavs/example.wav'
-samplerate = 22050
-fs = 250
-t = np.linspace(10, 1.2, samplerate)
+
+samplerate = 44100
+# The sampling frequency or sampling rate, fs, is the number of samples divided by the interval length over in which occur, thus fs = 1/T, with the unit sample per second, sometimes referred to as hertz, for example e.g. 48 kHz is 48,000 samples per second.
+
+fs = 300 # Hz
+# The hertz (symbol: Hz) is the unit of frequency in the International System of Units, equivalent to one cycle per second
+
 amplitude = np.iinfo(np.int16).max
-data = amplitude * np.sin(2. * np.pi * fs * t)
-write(wav_filename, samplerate, data.astype(np.int16))
+# In a standing wave, the amplitude of vibration has nulls at some positions where the wave amplitude appears smaller or even zero.
+
+time = np.linspace(start=0., stop=1., num=samplerate)
+# Returns num evenly spaced samples, calculated over the interval [start, stop].
+
+# how to change the duration?
+data = amplitude * np.sin(2 * np.pi * fs * time)
+write(wav_filename, samplerate, data)
+playsound(wav_filename)
+
+# plot written wav
+length = data.shape[0] / samplerate
+time = np.linspace(0., length, data.shape[0])
+plt.plot(time, data)
+plt.legend()
+plt.xlabel("Time [s]")
+plt.ylabel("Amplitude")
+plt.show()
+
 
 # %% read wav
 wav_filename = 'wavs/my808kit/snare2.wav'
@@ -24,7 +47,10 @@ print(f"number of channels = {data.shape[1]}")
 length = data.shape[0] / samplerate
 print(f"length = {length}s")
 
-# plot wav
+# write read wav
+write(wav_filename+'test.wav', samplerate, data)
+
+#  plot read wav
 time = np.linspace(0., length, data.shape[0])
 plt.plot(time, data[:, 0], label="Left channel")
 plt.plot(time, data[:, 1], label="Right channel")
@@ -33,14 +59,8 @@ plt.xlabel("Time [s]")
 plt.ylabel("Amplitude")
 plt.show()
 
-# %% play wav
-
-from playsound import playsound
-playsound(wav_filename)
-
 # %% plot a sine with matplotlib and numpy
 
-import matplotlib.pylab as plt
 x = np.linspace(-np.pi, np.pi, 201)
 plt.plot(x, np.sin(x))
 plt.xlabel('Angle [rad]')
@@ -58,25 +78,3 @@ Returns num evenly spaced samples, calculated over the interval [start, stop].
 
 The endpoint of the interval can optionally be excluded.
 '''
-
-from numpy import array
-np.linspace(2.0, 3.0, num=5)
-array([2.  , 2.25, 2.5 , 2.75, 3.  ])
-np.linspace(2.0, 3.0, num=5, endpoint=False)
-array([2. ,  2.2,  2.4,  2.6,  2.8])
-np.linspace(2.0, 3.0, num=5, retstep=True)
-(array([2.  ,  2.25,  2.5 ,  2.75,  3.  ]), 0.25)
-
-
-import matplotlib.pyplot as plt
-N = 8
-y = np.zeros(N)
-x1 = np.linspace(0, 10, N, endpoint=True)
-x2 = np.linspace(0, 10, N, endpoint=False)
-plt.plot(x1, y, 'o')
-
-plt.plot(x2, y + 0.5, 'o')
-
-plt.ylim([-0.5, 1])
-(-0.5, 1)
-plt.show()
