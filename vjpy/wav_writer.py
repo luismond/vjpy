173d36cafb9acb062f2e743c35d1a79a833e8caa
Luis Mondragon <luismond@gmail.com>
2023-07-29 03:10:01 -0600

    read wav with scipy

--- vjpy/wav_writer.py
+++ vjpy/wav_writer.py
@@ -4,19 +4,39 @@
 
 from scipy.io.wavfile import write
 import numpy as np
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
 
+# write wav
 # create a 100Hz sine wave, sampled at 44100Hz. Write to 16-bit PCM, Mono.
+wav_filename = 'wavs/example.wav'
 samplerate = 22050
 fs = 250
 t = np.linspace(10, 1.2, samplerate)
 amplitude = np.iinfo(np.int16).max
 data = amplitude * np.sin(2. * np.pi * fs * t)
-write("example.wav", samplerate, data.astype(np.int16))
+write(wav_filename, samplerate, data.astype(np.int16))
 
-# %% playsound
+# %% read wav
+wav_filename = 'wavs/my808kit/snare2.wav'
+samplerate, data = wavfile.read(wav_filename)
+print(f"number of channels = {data.shape[1]}")
+length = data.shape[0] / samplerate
+print(f"length = {length}s")
+
+# plot wav
+time = np.linspace(0., length, data.shape[0])
+plt.plot(time, data[:, 0], label="Left channel")
+plt.plot(time, data[:, 1], label="Right channel")
+plt.legend()
+plt.xlabel("Time [s]")
+plt.ylabel("Amplitude")
+plt.show()
+
+# %% play wav
 
 from playsound import playsound
-playsound("example.wav")
+playsound(wav_filename)
 
 # %% plot a sine with matplotlib and numpy
 
