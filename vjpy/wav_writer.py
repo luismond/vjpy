48a3eea84e734d3247e1fc6ebec9984695abd2e5
Luis Mondragon <luismond@gmail.com>
2023-07-29 03:00:51 -0600

    plot waves with numpy, scipy and matplotlib

--- vjpy/wav_writer.py
+++ vjpy/wav_writer.py
@@ -1,29 +1,62 @@
 """WavWriter"""
 
+# %% scipy.io.wavfile
+
 from scipy.io.wavfile import write
 import numpy as np
+
+# create a 100Hz sine wave, sampled at 44100Hz. Write to 16-bit PCM, Mono.
+samplerate = 22050
+fs = 250
+t = np.linspace(10, 1.2, samplerate)
+amplitude = np.iinfo(np.int16).max
+data = amplitude * np.sin(2. * np.pi * fs * t)
+write("example.wav", samplerate, data.astype(np.int16))
+
+# %% playsound
+
 from playsound import playsound
+playsound("example.wav")
+
+# %% plot a sine with matplotlib and numpy
+
+import matplotlib.pylab as plt
+x = np.linspace(-np.pi, np.pi, 201)
+plt.plot(x, np.sin(x))
+plt.xlabel('Angle [rad]')
+plt.ylabel('sin(x)')
+plt.axis('tight')
+plt.show()
+
+# %% numpy.linspace
+'''
+numpy.linspace(start, stop, num=50, endpoint=True, retstep=False, dtype=None, axis=0)[source]
+
+Return evenly spaced numbers over a specified interval.
+
+Returns num evenly spaced samples, calculated over the interval [start, stop].
+
+The endpoint of the interval can optionally be excluded.
+'''
+
+from numpy import array
+np.linspace(2.0, 3.0, num=5)
+array([2.  , 2.25, 2.5 , 2.75, 3.  ])
+np.linspace(2.0, 3.0, num=5, endpoint=False)
+array([2. ,  2.2,  2.4,  2.6,  2.8])
+np.linspace(2.0, 3.0, num=5, retstep=True)
+(array([2.  ,  2.25,  2.5 ,  2.75,  3.  ]), 0.25)
 
 
-class WavWriter:
-    def __init__(self, wav_name):
-        self.wav_name = wav_name
-        self.samplerate = 44100
-        self.fs = 50  # ?
-        self.t = np.linspace(1, 6, self.samplerate)  # ?
-        self.amplitude = np.iinfo(np.int16).max  # ?
-        self.x = np.sin(10 * np.pi * self.fs * self.t)  # ?
-        self.data = self.amplitude * self.x  # ? s
-        self.k = self.data.astype(np.int16)  # ?
+import matplotlib.pyplot as plt
+N = 8
+y = np.zeros(N)
+x1 = np.linspace(0, 10, N, endpoint=True)
+x2 = np.linspace(0, 10, N, endpoint=False)
+plt.plot(x1, y, 'o')
 
-    def write_wav(self):
-        write(self.wav_name, self.samplerate, self.k)
-        print('wrote wav')
+plt.plot(x2, y + 0.5, 'o')
 
-    def play_wav(self):
-        playsound(self.wav_name)
-
-
-ss = WavWriter(wav_name='example.wav')
-ss.write_wav()
-ss.play_wav()
+plt.ylim([-0.5, 1])
+(-0.5, 1)
+plt.show()
