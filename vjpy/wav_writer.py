1ac7f4c9b32b7d307c416b17e381bbf696f6e2f6
Luis Mondragon <luismond@gmail.com>
2023-08-04 20:23:59 -0600

    formalize wav writer functions

--- vjpy/wav_writer.py
+++ vjpy/wav_writer.py
@@ -1,67 +1,78 @@
 """vjpy WavWriter."""
 
-#  scipy.io.wavfile
-
-from scipy.io.wavfile import write
-from scipy.io import wavfile
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.io.wavfile import write
+from scipy.io import wavfile
 from playsound import playsound
 
-#
-# write wav
-# create a 100Hz sine wave, sampled at 44100Hz.
-# Write to 16-bit PCM, Mono.
-WAV_FILENAME = 'wavs/example.wav'
 
-SAMPLE_RATE = 44100
+def create_sine_wave():
+    """
+    Write and plot a wave.
 
-FS = 300  # Hz
+    100Hz sine wave, sampled at 44100Hz.
+    Write to 16-bit PCM, Mono.
+    Plot with matplotlib
 
-amplitude = np.iinfo(np.int16).max
+    linspace returns num evenly spaced samples,
+    calculated over the interval [start, stop].
+    Todo: find out how to change the duration.
+    """
+    # write wav
+    WAV_FILENAME = 'wavs/sine_wave_example.wav'
+    SAMPLE_RATE = 44100
+    FS = 300  # Hz
+    amplitude = np.iinfo(np.int16).max
+    time = np.linspace(start=0., stop=1., num=SAMPLE_RATE)
+    data = amplitude * np.sin(2 * np.pi * FS * time)
+    write(WAV_FILENAME, SAMPLE_RATE, data)
 
-time = np.linspace(start=0., stop=1., num=SAMPLE_RATE)
-# Returns num evenly spaced samples, calculated over the interval [start, stop].
+    # play wav
+    input("This will play a loud sinewave! Continue?")
+    playsound(WAV_FILENAME)
 
-# how to change the duration?
-data = amplitude * np.sin(2 * np.pi * FS * time)
-write(WAV_FILENAME, SAMPLE_RATE, data)
-playsound(WAV_FILENAME)
-
-# plot written wav
-length = data.shape[0] / SAMPLE_RATE
-time = np.linspace(0., length, data.shape[0])
-plt.plot(time, data)
-plt.legend()
-plt.xlabel("Time [s]")
-plt.ylabel("Amplitude")
-plt.show()
+    # plot wav
+    length = data.shape[0] / SAMPLE_RATE
+    time = np.linspace(0., length, data.shape[0])
+    plt.plot(time, data)
+    plt.legend()
+    plt.xlabel("Time [s]")
+    plt.ylabel("Amplitude")
+    plt.show()
 
 
-#  read wav
-WAV_FILENAME = 'wavs/my808kit/snare2.wav'
-SAMPLE_RATE, data = wavfile.read(WAV_FILENAME)
-print(f"number of channels = {data.shape[1]}")
-length = data.shape[0] / SAMPLE_RATE
-print(f"length = {length}s")
+def test_wav_reading():
+    """Read a wav, rewrite it, plot it."""
+    WAV_FILENAME = 'wavs/my808kit/snare.wav'
+    SAMPLE_RATE, data = wavfile.read(WAV_FILENAME)
+    print(f"number of channels = {data.shape[1]}")
+    length = data.shape[0] / SAMPLE_RATE
+    print(f"length = {length}s")
 
-# write read wav
-write(WAV_FILENAME+'test.wav', SAMPLE_RATE, data)
+    # re-write read wav
+    write(f"{WAV_FILENAME}_test.wav", SAMPLE_RATE, data)
 
-#  plot read wav
-time = np.linspace(0., length, data.shape[0])
-plt.plot(time, data[:, 0], label="Left channel")
-plt.plot(time, data[:, 1], label="Right channel")
-plt.legend()
-plt.xlabel("Time [s]")
-plt.ylabel("Amplitude")
-plt.show()
+    #  plot read wav
+    time = np.linspace(0., length, data.shape[0])
+    plt.plot(time, data[:, 0], label="Left channel")
+    plt.plot(time, data[:, 1], label="Right channel")
+    plt.legend()
+    plt.xlabel("Time [s]")
+    plt.ylabel("Amplitude")
+    plt.show()
 
-#  plot a sine with matplotlib and numpy
 
-x = np.linspace(-np.pi, np.pi, 201)
-plt.plot(x, np.sin(x))
-plt.xlabel('Angle [rad]')
-plt.ylabel('sin(x)')
-plt.axis('tight')
-plt.show()
+def plot_sine():
+    """Plot a sine with matplotlib and numpy."""
+    x = np.linspace(-np.pi, np.pi, 201)
+    plt.plot(x, np.sin(x))
+    plt.xlabel('Angle [rad]')
+    plt.ylabel('sin(x)')
+    plt.axis('tight')
+    plt.show()
+
+
+test_wav_reading()
+create_sine_wave()
+plot_sine()
