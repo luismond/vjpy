b9817132b5e3623dc1fe510aa48e078b828f6753
Luis Mondragon <luismond@gmail.com>
2023-08-05 01:56:19 -0600

    code quality review

--- vjpy_controller.py
+++ vjpy_controller.py
@@ -1,53 +1,54 @@
 """vjpy controller."""
-from time import sleep
 
 # devices
-from vjpy.midi_device import MidiDevice
-from vjpy.wav_device import WavDevice
+from vjpy.devices.midi_device import MidiDevice
+from vjpy.devices.wav_device import WavDevice
 
 # data
 from vjpy.data.midi.drumkits import TR808EmulationKit, my808kit
 from vjpy.data.midi.patterns import bar_example, bars_example
 
-# %% midi device
+# initialize devices
 md = MidiDevice(drumkit=TR808EmulationKit)
+wd = WavDevice(drumkit=my808kit)
 
-# %% Play a pattern
+# %% Test midi device
+
+# Play a pattern
+print("Playing a pattern.")
 md.play_pattern("k.h.c.h.")
+
 # Play a bar of patterns
-print(f"Playing a bar:\n{bar_example.patterns}\n")
+print(f"Playing a bar:\n{bar_example.patterns}")
 md.play_bar(bar_example)
+
 # Loop a bar
 print("Looping a bar")
-md.loop_bar(bars_example[0], num_loops=4)
+md.loop_bar(bars_example[0], num_loops=2)
+
 # Loop a sequence of bars
-print("Looping a sequence of bars\n")
-md.loop_bars(bars_example, num_loops=2)
+print("Looping a sequence of bars")
+md.loop_bars(bars_example, num_loops=1)
+
 # Generate random patterns
 for _ in range(4):
     rp = md.generate_random_pattern(patt_len=4)
-    print(f"Playing random pattern:\n{rp}\n")
+    print(f"Playing random pattern:{rp}")
     md.play_pattern(rp)
 
-# %% wav device
-wd = WavDevice(drumkit=my808kit)
+# %% Test midi receiving
+midi_receiver = md.open_midi_receiver()
+for m in md.yield_midi_msg(midi_receiver):
+    wd.play_wav_from_midi_msg(m)
+
+# %% Test midi sending
+midi_sender = md.open_midi_out()
+for _ in range(4):
+    for note in [43, 38, 43, 45]:
+        md.send_note(note)
+
+# %% Test wav device
 wd.create_sine_wave()
 wd.plot_sine()
 wd.test_wav_reading()
 wd.write_concatenated_wavs()
-
-# %% Test midi receiver and wav player
-midi_receiver = md.open_midi_receiver()
-wd = WavDevice(drumkit=my808kit)
-
-# Play each midi message received.
-for m in md.yield_midi_msg(midi_receiver):
-    wd.play_wav_from_midi_msg(m)
-
-# %% Test midi sender
-midi_sender = md.open_midi_out()
-
-for _ in range(4):
-    for note in [43, 38, 43, 45]:
-        md.send_note(note)
-        sleep(.5)
