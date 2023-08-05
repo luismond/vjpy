6a52449f07129497279168d7fc2f6fde3b0f1477
Luis Mondragon <luismond@gmail.com>
2023-08-04 21:53:54 -0600

    del local setup bash script

--- vjpy-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt install pyqt5-dev-tools -y
-sudo apt install imagemagick
-
-#update libs
-xmodmap -e "keycode 20 = underscore minus"
-xmodmap -e "keycode 47 = colon semicolon"
-xmodmap -e "keycode 48 = quotedbl apostrophe quotedbl apostrophe"
