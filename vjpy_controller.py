b1a3f86ce9efea236818b31afacfee30097a4daa
Luis Mondragon <luismond@gmail.com>
2023-08-28 19:40:47 -0600

    add video sequence example

--- vjpy_controller.py
+++ vjpy_controller.py
@@ -40,7 +40,7 @@ for msg in vjpd.yield_midi_msg():
 # %% Test midi sending
 vjpd.send_note(40)
 
-# %% Test video
+# %% Test video 1
 
 video_data = {
     "video_filename": 'test.mp4',
@@ -64,9 +64,110 @@ subclips = [sc_0, sc_0, sc_1, sc_0,
             sc_0, sc_0, sc_1, sc_0,
             sc_0, sc_0, sc_1, sc_2, sc_2,
             sc_0, sc_0, sc_1, sc_0,
+            sc_0, sc_0, sc_1, sc_3, sc_3, sc_3, sc_3,
+            sc_0, sc_0, sc_1, sc_0,
+            sc_0, sc_0, sc_1, sc_2, sc_2,
+            sc_0, sc_0, sc_1, sc_0,
+            sc_0, sc_0, sc_1, sc_3, sc_3, sc_3, sc_3,
+            sc_0, sc_0, sc_1, sc_0,
+            sc_0, sc_0, sc_1, sc_2, sc_2,
+            sc_0, sc_0, sc_1, sc_0,
             sc_0, sc_0, sc_1, sc_3, sc_3, sc_3, sc_3]
 
 
 c_subclips = vjpd.concatenate_subclips(subclips)
 
 vjpd.write_concatenated_subclips(c_subclips, video_data['subclip_name'])
+
+# %% Test video 2
+import os
+d = '/home/user/Downloads/vjpy_video_tests'
+video_filenames = sorted([v for v in os.listdir(d)])
+
+#%%
+video_clips = []
+for v in video_filenames:
+    vp = os.path.join(d, v)
+    video_clip = vjpd.get_video_clip(vp)
+    video_clips.append(video_clip)
+
+#%%
+
+# hh_pedal_1
+a = vjpd.get_video_subclip(video_clips[0], 10.28, 10.53)
+
+# hh_pedal_2
+b = vjpd.get_video_subclip(video_clips[6], 6.23, 6.48)
+
+# hh_pedal_3
+b2 = vjpd.get_video_subclip(video_clips[6], 6.23, 6.355)
+
+# tom_1
+c = vjpd.get_video_subclip(video_clips[6], 20.3, 20.55)
+
+# tom_2
+c2 = vjpd.get_video_subclip(video_clips[6], 20.30, 20.425)
+
+# crash
+d = vjpd.get_video_subclip(video_clips[7], 10.3, 10.55)
+
+# snare_bell
+e = vjpd.get_video_subclip(video_clips[10], 6.10, 6.35)
+
+# snare_crash_1
+f = vjpd.get_video_subclip(video_clips[6], 16.9, 17.15)
+
+# snare
+g = vjpd.get_video_subclip(video_clips[6], 20.73, 20.98)
+
+def loop_patt(patt, n):
+    return patt*n
+
+patt = [
+        c, b, g, b, #1
+        c, b, g, b, 
+        c, b, g, c,
+        c, c, f, c,
+        
+        c, b, g, b, #2
+        c, b, g, b, 
+        c, b, g, c,
+        c, c, f, e,
+        
+        c, b, g, b, #3
+        c, b, g, b, 
+        c, b, g, c,
+        c, c, f, c,
+        
+        c, b, g, b, #4
+        c, b, g, b, 
+        c, b, g, c,
+        c2, c2, c2, c2, g, g,
+        
+        c, b, g, b, #5
+        c, b, g, b, 
+        c, b, g, c,
+        c, c, f, c,
+        
+        c, b, g, b, #6
+        c, b, g, b, 
+        c, b, g, c,
+        c, c, f, f,
+        
+        c, b, g, b, #7
+        c, b, g, b, 
+        c, b, g, c,
+        c, c, f, c,
+        
+        c, b, g, b, #8
+        c, b, g, b, 
+        c, b, g, c,
+        d, d, c2, c2, c2, c2, d
+        
+        ]
+
+patt_l = loop_patt(patt, 1)
+
+c_subclips = vjpd.concatenate_subclips(patt_l)
+
+vjpd.write_concatenated_subclips(c_subclips, 'subclip_test.mp4')
