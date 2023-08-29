20073e3154481127cf0e6a52e094848a26df1591
Luis Mondragon <luismond@gmail.com>
2023-08-28 22:10:47 -0600

    add example 3 with new video sound bank

--- vjpy_controller.py
+++ vjpy_controller.py
@@ -111,6 +111,9 @@ c2 = vjpd.get_video_subclip(video_clips[6], 20.30, 20.425)
 # crash
 d = vjpd.get_video_subclip(video_clips[7], 10.3, 10.55)
 
+# crash2
+d2 = vjpd.get_video_subclip(video_clips[7], 10.3, 10.8)
+
 # snare_bell
 e = vjpd.get_video_subclip(video_clips[10], 6.10, 6.35)
 
@@ -127,47 +130,105 @@ patt = [
         c, b, g, b, #1
         c, b, g, b, 
         c, b, g, c,
-        c, c, f, c,
+        c, c, e, c,
         
         c, b, g, b, #2
         c, b, g, b, 
         c, b, g, c,
-        c, c, f, e,
+        c, c, e, e,
         
         c, b, g, b, #3
         c, b, g, b, 
         c, b, g, c,
-        c, c, f, c,
+        c, c, e, c,
         
         c, b, g, b, #4
         c, b, g, b, 
         c, b, g, c,
         c2, c2, c2, c2, g, g,
         
-        c, b, g, b, #5
+        c, b, g, b, #1b
         c, b, g, b, 
         c, b, g, c,
-        c, c, f, c,
+        c, c, e, c,
         
-        c, b, g, b, #6
+        c, b, g, b, #2b
         c, b, g, b, 
         c, b, g, c,
-        c, c, f, f,
+        e, e, e, e,
         
-        c, b, g, b, #7
+        c, b, g, b, #3b
         c, b, g, b, 
         c, b, g, c,
-        c, c, f, c,
+        c, c, e, c,
         
-        c, b, g, b, #8
+        c, b, g, b, #4b
         c, b, g, b, 
-        c, b, g, c,
-        d, d, c2, c2, c2, c2, d
+        c, b, g, g,
+        c2, c2, c2, c2, g, g, d2
         
         ]
 
 patt_l = loop_patt(patt, 1)
 
 c_subclips = vjpd.concatenate_subclips(patt_l)
+
+vjpd.write_concatenated_subclips(c_subclips, 'subclip_test.mp4')
+#%%
+
+# %% Test video 3
+import os
+d = 'robodrum-bank'
+video_filenames = sorted([v for v in os.listdir(d)])
+
+#%%
+video_clips = []
+for v in video_filenames:
+    vp = os.path.join(d, v)
+    video_clip = vjpd.get_video_clip(vp)
+    video_clips.append(video_clip)
+    
+#%%
+clip_duration = .25
+
+
+# sticks
+a_clip = video_clips[1]
+a_clip_start = 1.6
+a = vjpd.get_video_subclip(a_clip, a_clip_start, a_clip_start+clip_duration)
+
+# hat sticked
+b_clip = video_clips[4]
+b_clip_start = 18.45
+b = vjpd.get_video_subclip(b_clip, b_clip_start, b_clip_start+clip_duration)
+
+# crash
+c_clip = video_clips[4]
+c_clip_start = 21
+c = vjpd.get_video_subclip(c_clip, c_clip_start, c_clip_start+clip_duration)
+
+# rim
+d_clip = video_clips[4]
+d_clip_start = 25.9
+d = vjpd.get_video_subclip(d_clip, d_clip_start, d_clip_start+clip_duration)
+
+# hat open
+e_clip = video_clips[5]
+e_clip_start = 20.8
+e = vjpd.get_video_subclip(e_clip, e_clip_start, e_clip_start+clip_duration)
+
+# tom
+f_clip = video_clips[6]
+f_clip_start = 20.13
+f = vjpd.get_video_subclip(f_clip, f_clip_start, f_clip_start+clip_duration)
+
+# snare
+g_clip = video_clips[10]
+g_clip_start = 20.745
+g = vjpd.get_video_subclip(g_clip, g_clip_start, g_clip_start+clip_duration)
+
+
+
+c_subclips = vjpd.concatenate_subclips([g])
 
 vjpd.write_concatenated_subclips(c_subclips, 'subclip_test.mp4')
