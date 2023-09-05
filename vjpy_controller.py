7b33ff9900cb8f43d2b047d32aa014f95e0f986c
Luis Mondragon <luismondragon@protonmail.com>
2023-09-05 02:17:36 -0600

    implement outputting video to clip array

--- vjpy_controller.py
+++ vjpy_controller.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """vjpy controller."""
 import os
+from moviepy.editor import CompositeVideoClip, VideoFileClip, clips_array, vfx
 from vjpy import VjPyDevice, Bar
 vjpd = VjPyDevice()
 # vjpd.write_concatenated_wavs(['k','h','c','h'])         # Concat wavs
@@ -15,92 +16,116 @@ vjpd = VjPyDevice()
 # vjpd.send_note(40)                                      # Send MIDI note
 
 #%%
-soundbank_name = 'drums_06'
+soundbank_name = 'drums_03'
 soundbank_dir_path = os.path.join(vjpd.soundbanks_path, soundbank_name)
 soundbank_video_path = os.path.join(soundbank_dir_path, f'{soundbank_name}.mp4')
 soundbank_videoclip = vjpd.get_videoclip(soundbank_video_path)
 
-dur = .3
+dur = 0.15
 
-# chunks
-a = vjpd.get_videosubclip(soundbank_videoclip, start=02.1120, duration=dur)
-b = vjpd.get_videosubclip(soundbank_videoclip, start=02.6340, duration=dur)
-c = vjpd.get_videosubclip(soundbank_videoclip, start=03.1240, duration=dur)
-d = vjpd.get_videosubclip(soundbank_videoclip, start=03.6450, duration=dur)
+b = vjpd.get_videosubclip(soundbank_videoclip, start=00.100, duration=dur) # bell
+r = vjpd.get_videosubclip(soundbank_videoclip, start=03.710, duration=dur) # ride
+x = vjpd.get_videosubclip(soundbank_videoclip, start=07.137, duration=dur) # china
+c = vjpd.get_videosubclip(soundbank_videoclip, start=09.770, duration=dur) # crash1
+d = vjpd.get_videosubclip(soundbank_videoclip, start=13.220, duration=dur) # crash2
+i = vjpd.get_videosubclip(soundbank_videoclip, start=17.800, duration=dur) # hat1 pedal
+j = vjpd.get_videosubclip(soundbank_videoclip, start=19.390, duration=dur) # hat2
+h = vjpd.get_videosubclip(soundbank_videoclip, start=21.290, duration=dur) # hat3
+y = vjpd.get_videosubclip(soundbank_videoclip, start=23.700, duration=dur) # hat5
+o = vjpd.get_videosubclip(soundbank_videoclip, start=23.113, duration=dur) # hat4 open
+k = vjpd.get_videosubclip(soundbank_videoclip, start=24.950, duration=dur) # kick1
+l = vjpd.get_videosubclip(soundbank_videoclip, start=26.050, duration=dur) # kick2
+s = vjpd.get_videosubclip(soundbank_videoclip, start=27.513, duration=dur) # snare1
+z = vjpd.get_videosubclip(soundbank_videoclip, start=29.512, duration=dur) # snare2
+t = vjpd.get_videosubclip(soundbank_videoclip, start=31.505, duration=dur) # tom1a
+v = vjpd.get_videosubclip(soundbank_videoclip, start=34.248, duration=dur) # tom1b
+w = vjpd.get_videosubclip(soundbank_videoclip, start=37.160, duration=dur) # tom2a
+u = vjpd.get_videosubclip(soundbank_videoclip, start=39.010, duration=dur) # tom2b
+_ = vjpd.get_videosubclip(soundbank_videoclip, start=01.500, duration=dur) # silence
 
-e = vjpd.get_videosubclip(soundbank_videoclip, start=04.1694, duration=dur)
-f = vjpd.get_videosubclip(soundbank_videoclip, start=04.6664, duration=dur)
-g = vjpd.get_videosubclip(soundbank_videoclip, start=05.1600, duration=dur)
-h = vjpd.get_videosubclip(soundbank_videoclip, start=05.6600, duration=dur)
+# patterns and bars
 
-i = vjpd.get_videosubclip(soundbank_videoclip, start=06.1514, duration=dur)
-j = vjpd.get_videosubclip(soundbank_videoclip, start=06.6787, duration=dur)
-k = vjpd.get_videosubclip(soundbank_videoclip, start=07.1909, duration=dur)
-l = vjpd.get_videosubclip(soundbank_videoclip, start=07.6833, duration=dur)
+# ...........................................................................................
 
-m = vjpd.get_videosubclip(soundbank_videoclip, start=08.1850, duration=dur)
-n = vjpd.get_videosubclip(soundbank_videoclip, start=08.7360, duration=dur)
+##         |01   02   03   04   05   06   07   08   09   10   11   12   13   14   15   16   
+#patt_00 = [_,   _,   _,   _,   _,   _,   _,   _,   _,   _,   _,   _,   _,   _,   _,   _,   ]
 
+# ...........................................................................................
 
-# PATTERNS 
-          # 12345678
-patt_01 = [a,b,c,d]
-patt_02 = [a,a,c,d]
-patt_03 = [a,b,c,d]
-patt_04 = [a,a,c,e]
+#         |01   02   03   04   05   06   07   08   09   10   11   12   13   14   15   16   
+patt_01 = [_,   _,   _,   _,   _,   _,   _,   _,   _,   _,   _,   _,   _,   _,   _,   _,   ]
 
-patt_05 = [a,b,c,d]
-patt_06 = [a,a,c,d]
-patt_07 = [a,b,c,d]
-patt_08 = [a,a,g,h]
+#         |01   02   03   04   05   06   07   08   09   10   11   12   13   14   15   16   
+patt_02 = [k,   k,   _,   _,   s,   _,   _,   _,   _,   _,   _,   _,   z,   _,   k,   _,   ]
 
-patt_09 = [g,h,g,h]
+#         |01   02   03   04   05   06   07   08   09   10   11   12   13   14   15   16   
+patt_03 = [_,   _,   r,   _,   _,   _,   r,   _,   _,   _,   r,   _,   _,   _,   r,   _,   ]
 
+#         |01   02   03   04   05   06   07   08   09   10   11   12   13   14   15   16   
+patt_04 = [_,   _,   _,   _,   _,   _,   _,   _,   _,   _,   _,   _,   _,   _,   _,   u,   ]
+
+# ...........................................................................................
+
+#         |01   02   03   04   05   06   07   08   09   10   11   12   13   14   15   16   
+patt_05 = [h,   _,   h,   _,   j,   _,   h,   _,   h,   h,   h,   _,   j,   _,   o,   _    ]
+
+#         |01   02   03   04   05   06   07   08   09   10   11   12   13   14   15   16   
+patt_06 = [k,   k,   k,   _,   s,   _,   _,   _,   _,   _,   _,   _,   z,   _,   k,   _,   ]
+
+#         |01   02   03   04   05   06   07   08   09   10   11   12   13   14   15   16   
+patt_07 = [_,   _,   _,   _,   _,   _,   _,   _,   _,   _,   _,   x,   _,   _,   c,   c,   ]
+
+#         |01   02   03   04   05   06   07   08   09   10   11   12   13   14   15   16   
+patt_08 = [_,   _,   _,   _,   _,   _,   v,   v,   _,   _,   _,   _,   w,   u,   w,   u,   ]
+
+# ...........................................................................................
 
 # BARS
-        # |1 . . . |2 . . . |3 . . . |4 . . . 
-bar_01 = [patt_01, patt_02, patt_03, patt_04 ]
-bar_02 = [patt_05, patt_06, patt_07, patt_08 ]
-#bar_03 = [patt_05, patt_06, patt_09, patt_09 ]
+        # |1 . . .|2 . . . |3 . . . |4 . . . 
+bar_01 = [patt_01, patt_05, patt_05, patt_05, patt_01, patt_05, patt_05, patt_05]
+        # |1 . . . |2 . . . |3 . . . |4 . . .
+bar_02 = [patt_02, patt_06, patt_02, patt_06, patt_02, patt_06, patt_02, patt_06]
+        # |1 . . . |2 . . . |3 . . . |4 . . .
+bar_03 = [patt_01, patt_01, patt_03, patt_03, patt_03, patt_03, patt_03, patt_03]
+        # |1 . . . |2 . . . |3 . . . |4 . . .
+bar_04 = [patt_01, patt_01, patt_04, patt_04, patt_04, patt_04, patt_04, patt_08]
 
-
-bars = [
-        bar_01, bar_01, bar_01, bar_02,
-        bar_01, bar_01, bar_01, bar_02,]
-        # bar_01, bar_02, bar_01, bar_03,
-        # ]
+bars_d = {
+        'hats': 
+            [bar_01],
+        'kick_snare':
+            [bar_02],
+        'cymbs':
+            [bar_03],
+        'toms':
+            [bar_04]
+        }
 
 # Make and save final clip
-subclips = vjpd.get_bars_subclips(bars)
-final_clip = vjpd.concatenate_subclips(subclips)
-final_clip_path = os.path.join(soundbank_dir_path, 'beats', 'beat_13.mp4')
-vjpd.write_concatenated_subclips(final_clip, final_clip_path)
+for drum_name, bars in bars_d.items():
+    bars = bars*2
+    beat_n = '14'
+    print(drum_name)
+    subclips = vjpd.get_bars_subclips(bars)
+    final_clip = vjpd.concatenate_subclips(subclips)
+    final_clip_path = os.path.join(soundbank_dir_path, 'beats', f'{beat_n}', f'{drum_name}.mp4')
+    vjpd.write_concatenated_subclips(final_clip, final_clip_path)
 
 
-#%%
+##%% compositing
 
-from moviepy.editor import CompositeVideoClip, VideoFileClip, clips_array, vfx
 
-soundbank_name = 'drums_05'
-soundbank_dir_path = os.path.join(vjpd.soundbanks_path, soundbank_name)
-soundbank_video_path = os.path.join(soundbank_dir_path, f'{soundbank_name}.mp4')
 
-#
-clip1 = VideoFileClip(soundbank_video_path)
-clip2 = clip1.fx( vfx.mirror_x)
-clip3 = clip1.fx( vfx.mirror_y)
-clip4 = clip3.fx( vfx.mirror_y)
+clip1 = VideoFileClip(os.path.join(soundbank_dir_path, 'beats', f'{beat_n}', 'hats.mp4'))
+clip1 = clip1.fx(vfx.mirror_x)
+clip2 = VideoFileClip(os.path.join(soundbank_dir_path, 'beats', f'{beat_n}', 'kick_snare.mp4'))
 
-final_clip = clips_array([[clip1, clip2],
-                          [clip3, clip4]])
-final_clip.resize(width=960).write_videofile("my_stack.mp4")
+clip3 = VideoFileClip(os.path.join(soundbank_dir_path, 'beats', f'{beat_n}', 'cymbs.mp4'))
+clip3 = clip3.fx(vfx.mirror_x)
 
-#%%
+clip4 = VideoFileClip(os.path.join(soundbank_dir_path, 'beats', f'{beat_n}', 'toms.mp4'))
 
-video = CompositeVideoClip([clip1,clip2,clip3,clip4], size=(720,460))
-video.resize(width=960).write_videofile("my_comp.mp4")
-#%%
-video = CompositeVideoClip([clip1,
-                           clip2.set_position((45,150)),
-                           clip3.set_position((90,100))])
+video = clips_array([[clip1, clip2],
+                     [clip3, clip4]])
+
 video.resize(width=960).write_videofile("my_comp.mp4")
