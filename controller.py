c7d3afe82eba05d2b1c4ba9a2e6c87dd1808dd41
Luis Mondragon <luismond@gmail.com>
2023-09-23 07:20:49 -0600

    remove patterns file

--- controller.py
+++ controller.py
@@ -1,6 +1,6 @@
 """vjpy controller."""
 import os
-from vjpy import VjPyDevice, MidiDevice, WavDevice, VideoDevice, patterns
+from vjpy import VjPyDevice, MidiDevice, WavDevice, VideoDevice
 
 vj = VjPyDevice(bpm=100)
 
@@ -8,35 +8,40 @@ md = MidiDevice(vj)
 wd = WavDevice(vj)
 vd = VideoDevice(vj, bankname="drums_03", beatname="15")
 
-# Play MIDI note
-md.play_note(note=44, velocity=120, duration=0)
-# Play MIDI patterns
-md.play_patterns(patterns)
+# Play MIDI notes
+md.play_note(note=44)
+
 # Play MIDI file
+print('playing midi file')
 filename = os.path.join(md.midi_data_dir, "drum_beat.mid")
 md.play_midi_file(filename)
 
 # Play wav
+print('playing snare.wav')
 wav_names = ["snare.wav", "hat.wav", "kick.wav"]
 wav_paths = [os.path.join(wd.drumkit_dir, wav_name) for wav_name in wav_names]
 wd.play_wav(wav_paths[0])
+
 # Concatenate wavs
+print('playing concat wavs')
 wav_concat = wd.concatenate_wavs(wav_paths)
 concat_wav_path = os.path.join(wd.wav_dir, "examples", "concat_wavs.wav")
 wd.write_wav(concat_wav_path, wav_concat)
 wd.play_wav(concat_wav_path)
+
 # Mix wavs
+print('playing mixed wavs')
 wav_mixed = wd.mix_wavs(wav_paths)
 mixed_wav_path = os.path.join(wd.wav_dir, "examples", "mixed_wavs.wav")
 wd.write_wav(mixed_wav_path, wav_mixed)
 wd.play_wav(mixed_wav_path)
 
-# Render wav pattern
-steps = wd.wav_patterns_to_steps(patterns)
-patt_concat = wd.render_wav_steps(steps)
-mixed_wav_path = os.path.join(wd.wav_dir, "examples", "rendered_wav_pattern.wav")
-wd.write_wav(mixed_wav_path, patt_concat)
-wd.play_wav(mixed_wav_path)
+#%% Render wav pattern
+# steps = wd.wav_patterns_to_steps(patterns)
+# patt_concat = wd.render_wav_steps(steps)
+# mixed_wav_path = os.path.join(wd.wav_dir, "examples", "rendered_wav_pattern.wav")
+# wd.write_wav(mixed_wav_path, patt_concat)
+# wd.play_wav(mixed_wav_path)
 
 # Parse a MIDI file, render a target wav and play it
 filename = os.path.join(md.midi_data_dir, "drum_beat.mid")
@@ -60,9 +65,9 @@ subclip = vd.get_subclip(videoclip, start=03.710) # ride
 # Define a video drum kit
 vdk = vd.get_vdk(videoclip)
 # Concatenate drum subclips acordding to patterns
-vd.concat_drum_subpatterns(patterns, vdk, loops_n=2)
+#vd.concat_drum_subpatterns(patterns, vdk, loops_n=2)
 #  Composite a polyphonic, vertical video array from the concatenated drum subclips
-vd.composite_vertical_videobeat(patterns)
+#vd.composite_vertical_videobeat(patterns)
 
 # Read a MIDI file, render a target video
 filename = os.path.join(md.midi_data_dir, "drum_beat.mid")
