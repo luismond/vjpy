fde12baba18fbca8b2f964774f55c6380590e8ee
Luis Mondragon <luismond@gmail.com>
2023-11-27 18:40:46 -0600

    add rudiments

--- controller.py
+++ controller.py
@@ -10,32 +10,32 @@ vd = VideoDevice(vj)
 
 # Play MIDI file
 print('playing midi file')
-fn = "triplets.mid"
+fn = "paradiddles.mid"
 # fn = "hits.mid"
 filename = os.path.join(md.midi_data_dir, fn)
 msgs = md.get_sorted_midi_messages(filename)
 midi_steps = md.get_midi_steps(msgs)
-md.play_midi_file(filename)
+#md.play_midi_file(filename)
 
 # Play wav
 wav_names = ["snare.wav", "hh1.wav", "kick.wav"]
 wav_paths = [os.path.join(wd.drumkit_dir, wav_name) for wav_name in wav_names]
 print('playing snare.wav')
-wd.play_wav(wav_paths[0])
+#wd.play_wav(wav_paths[0])
 
 # # Concatenate wavs
 print('playing concat wavs')
 wav_concat = wd.concatenate_wavs(wav_paths)
 concat_wav_path = os.path.join(wd.wav_dir, "examples", "concat_wavs.wav")
 wd.write_wav(concat_wav_path, wav_concat)
-wd.play_wav(concat_wav_path)
+#wd.play_wav(concat_wav_path)
 
 # # Mix wavs
 print('playing mixed wavs')
 wav_mixed = wd.mix_wavs(wav_paths)
 mixed_wav_path = os.path.join(wd.wav_dir, "examples", "mixed_wavs.wav")
 wd.write_wav(mixed_wav_path, wav_mixed)
-wd.play_wav(mixed_wav_path)
+#wd.play_wav(mixed_wav_path)
 
 # Parse a MIDI file, render a target wav and play it
 midi_patt_wav = wd.render_midi_steps(midi_steps)
