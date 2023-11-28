9c4a1408259db319f8e3ba67fc580f9babad2a2a
Luis Mondragon <luismond@gmail.com>
2023-11-28 04:02:18 -0600

    optimize code, add jazz file

--- controller.py
+++ controller.py
@@ -1,36 +1,37 @@
 """vjpy controller."""
 import os
 from vjpy import VjPyDevice, MidiDevice, WavDevice, VideoDevice
-
+from collections import defaultdict
+import mido, time
 vj = VjPyDevice()
 
 md = MidiDevice(vj)
 wd = WavDevice(vj)
 vd = VideoDevice(vj)
 
-# Play MIDI file
-print('playing midi file')
-#fn = "paradiddles.mid"
-fn = "hits.mid"
-filename = os.path.join(md.midi_data_dir, fn)
-msgs = md.get_sorted_midi_messages(filename)
-midi_steps = md.get_midi_steps(msgs)
-md.play_midi_file(filename)
+#%% Play MIDI file
+print('Playing midi file')
+fn = "jazz.mid"
+
+fp = os.path.join(md.midi_data_dir, fn)
+msgs = md.get_sorted_midi_messages(fp)
+steps = md.get_midi_steps(msgs)
+md.play_midi_file(fp)
 
 # Play wav
 wav_names = ["snare.wav", "hh1.wav", "kick.wav"]
 wav_paths = [os.path.join(wd.drumkit_dir, wav_name) for wav_name in wav_names]
 print('playing snare.wav')
-wd.play_wav(wav_paths[0])
+#wd.play_wav(wav_paths[0])
 
-# # Concatenate wavs
+# Concatenate wavs
 print('playing concat wavs')
 wav_concat = wd.concatenate_wavs(wav_paths)
 concat_wav_path = os.path.join(wd.wav_dir, "examples", "concat_wavs.wav")
 wd.write_wav(concat_wav_path, wav_concat)
 wd.play_wav(concat_wav_path)
 
-# # Mix wavs
+# Mix wavs
 print('playing mixed wavs')
 wav_mixed = wd.mix_wavs(wav_paths)
 mixed_wav_path = os.path.join(wd.wav_dir, "examples", "mixed_wavs.wav")
@@ -38,12 +39,12 @@ wd.write_wav(mixed_wav_path, wav_mixed)
 wd.play_wav(mixed_wav_path)
 
 # Parse a MIDI file, render a target wav and play it
-midi_patt_wav = wd.render_midi_steps(midi_steps)
+midi_patt_wav = wd.render_midi_steps(steps)
 midi_patt_wav_path = os.path.join(wd.wav_dir, "examples", "rendered_midi_pattern.wav")
 wd.write_wav(midi_patt_wav_path, midi_patt_wav)
 wd.play_wav(midi_patt_wav_path)
 
-# Onset detection
+#%% Onset detection
 print('Onset detection')
 filepath = os.path.join(wd.wav_dir, "examples", "drums_03.wav")
 peaks = wd.find_local_energy_peaks(filepath, prominence=3)
@@ -61,7 +62,7 @@ vdk = vd.get_vdk(videoclip)
 
 # Read a MIDI file, render a target video
 print('Rendering video beat from midi')
-patterns = vd.midi_steps_to_pattern(midi_steps, vdk)
+patterns = vd.midi_steps_to_pattern(steps, vdk)
 vd.concat_drum_subpatterns(
     patterns,
     vdk,
@@ -76,4 +77,4 @@ vd.composite_vertical_videobeat(
     )
 
 # Read a monophonic MIDI file, render a 1-array video
-vd.render_monophonic_video(vdk, videoclip, midi_steps)
+vd.render_monophonic_video(vdk, videoclip, steps)
