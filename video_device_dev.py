259feeb9ef97e8062df42c1e150f78261d66a6bf
Luis Mondragon <luismond@gmail.com>
2023-11-30 15:05:46 -0600

    use notes as vdk keys

--- video_device_dev.py
+++ video_device_dev.py
@@ -34,7 +34,6 @@ videoclip = VideoFileClip(SOUNDBANK_PATH)
 
 
 duration = list(steps.keys())[1]-list(steps.keys())[0]
-#duration = [(b-a) for (a, b) in list(zip(steps_start, steps_start[1:]))][0]
 
 
 def get_subclip(videoclip, start=0, duration=None):
@@ -48,52 +47,43 @@ def get_subclip(videoclip, start=0, duration=None):
 vdk = VideoDrumkit(
     name="videokit",
     drums={
-        'k': VideoDrum(name="kick", note=36, short_hand="k", start=24.950),
-        "s": VideoDrum(name="snare", note=38, short_hand="s", start=27.5128),
-        "z": VideoDrum(name="snare2", note=37, short_hand="z", start=29.5085),
-        "t": VideoDrum(name="tom1", note=45, short_hand="t", start=31.4610),
-        "w": VideoDrum(name="tom2", note=41, short_hand="w", start=38.9788),
-        "p": VideoDrum(name="tom3", note=43, short_hand="p", start=38.9788),
-        "r": VideoDrum(name='ride', note=51, short_hand="r", start=03.7158),
-        "x": VideoDrum(name="china", note=49, short_hand="x", start=07.1222),
-        "c": VideoDrum(name="crash", note=57, short_hand="c", start=09.7210),
-        "h": VideoDrum(name="hat", note=42, short_hand="h", start=21.2910),
-        "f": VideoDrum(name="hat", note=44, short_hand="f", start=21.2910),
-        "o": VideoDrum(name="hat_open", note=46, short_hand="o", start=23.0869),
-        "_": VideoDrum(name="silence", note=81, short_hand="_", start=06.005),
+        36: VideoDrum(name="kick", note=36, short_hand="k", start=24.950),
+        38: VideoDrum(name="snare", note=38, short_hand="s", start=27.5128),
+        37: VideoDrum(name="snare2", note=37, short_hand="z", start=29.5085),
+        45: VideoDrum(name="tom1", note=45, short_hand="t", start=31.4610),
+        41: VideoDrum(name="tom2", note=41, short_hand="w", start=38.9788),
+        43: VideoDrum(name="tom3", note=43, short_hand="p", start=38.9788),
+        51: VideoDrum(name='ride', note=51, short_hand="r", start=03.7158),
+        49: VideoDrum(name="china", note=49, short_hand="x", start=07.1222),
+        57: VideoDrum(name="crash", note=57, short_hand="c", start=09.7210),
+        42: VideoDrum(name="hat", note=42, short_hand="h", start=21.2910),
+        44: VideoDrum(name="hat", note=44, short_hand="f", start=21.2910),
+        46: VideoDrum(name="hat_open", note=46, short_hand="o", start=23.0869),
+        81: VideoDrum(name="silence", note=81, short_hand="_", start=06.005),
         }
     )
 
-drumkit_note_shs = {}
-for drum in vdk.drums.values():
-    drumkit_note_shs[drum.note] = drum.short_hand
-
-'''
-{36: 'k',
-  38: 's',
-  37: 'z',
-  45: 't'}
-'''
+# drumkit_note_shs = {}
+# for drum in vdk.drums.values():
+#     drumkit_note_shs[drum.note] = drum#.short_hand
 
 
-# # # collect unique note shorthands
-shs = set()
-for s in steps.items():
-    notes = s[1]
+# # # collect unique notes
+notes_set = set()
+for s, notes in steps.items():
     for note in notes:
         if note != 81:
-            sh = drumkit_note_shs[note]
-            shs.add(sh)
+            notes_set.add(note)
 '''
 {'h', 'r', 'k', 's'}
 '''
 
 
-# # create empty pattern dictionary with the necessary drum keys
+# create empty pattern dictionary with the necessary drum keys
 pattern = defaultdict(list)
-for sh in shs:
+for note in notes_set:
     for n in range(len(steps.items())):
-        pattern[sh].append("_")
+        pattern[note].append("_")
 
 '''
 k : ['_', '_', '_', '_', '_', '_', ...]
@@ -105,14 +95,12 @@ for n, s in enumerate(steps.items()):
     notes = s[1]
     for note in notes:
         if note != 81:
-            sh = drumkit_note_shs[note]
-            pattern[sh][n] = "x"
+            pattern[note][n] = "x"
 
 '''
 k : ['x', '_', '_', '_', 'x', '_', ...]
 s : ['_', '_', 'x', '_', '_', '_', ...]
 '''
-
 
 key_clips = defaultdict(list)
 
@@ -121,7 +109,7 @@ for key, key_pattern in pattern.items():
         if hit == "x":
             key_clip = get_subclip(videoclip, start=vdk.drums[key].start, duration=duration)
         else:
-            key_clip = get_subclip(videoclip, start=vdk.drums['_'].start, duration=duration)
+            key_clip = get_subclip(videoclip, start=vdk.drums[81].start, duration=duration)
         key_clips[key].append(key_clip)
 
 for key in key_clips:
